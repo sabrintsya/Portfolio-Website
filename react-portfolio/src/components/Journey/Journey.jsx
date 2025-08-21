128bc15ca5f8354dcd06570320bb3e827b8d1199
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-08-21 13:32:14 +0700

    add for journey

---
+++ react-portfolio/src/components/Journey/Journey.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "../Journey/Journey.module.css";
+
+
+export const Journey = () => {
+  return (
+    <div>Journey</div>
+  )
+}
