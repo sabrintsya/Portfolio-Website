37f43b28292e5240698a72a8d1586f2aa4b31023
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-08-21 13:33:18 +0700

    add for journey

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 
 import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
+import { Journey } from "./components/Journey/Journey";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <Hero />
+    <Journey />
   </div>;
 };
 
