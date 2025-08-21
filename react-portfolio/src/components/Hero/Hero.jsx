78d0b76609b79f859f059f2dc8f2615b62772ab1
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-08-21 15:31:06 +0700

    update add journey section

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import styles from "./Hero.module.css";
 
 export const Hero = () => {
     return (
-        <section className={styles.container}>
+        <section className={styles.container} id="home">
             <div className={styles.content}>
                 <h1 className={styles.title} >Design,with Purpose.</h1>
                 <p className={styles.description}> Not just usable.<br />
