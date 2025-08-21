ae29f28186464e38668b7b5503dcf14d63ed0cca
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-08-21 15:39:57 +0700

    add portfolio section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,12 +3,14 @@ import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { Journey } from "./components/Journey/Journey";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Portfolio } from "./components/Portfolio/Portfolio";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <Hero />
     <Journey />
+    <Portfolio />
   </div>;
 };
 
---
+++ react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import styles from "./Portfolio.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
