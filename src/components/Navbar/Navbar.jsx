eec2590ebf5db67a5962d8dccc2140c4dce1e28e
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2026-06-15 10:06:03 +0700

    perubahan terhadap navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,25 @@
 import React, { useState, useEffect } from "react";
-
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
-
 import { Link } from "react-router-dom";
-
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
+    const handleScroll = () => setScrolled(window.scrollY > 50);
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <nav className={styles.navbar}>
-
-
-      <div className={`${styles.menu} ${scrolled ? styles.scrolled : ""}`}  >
-        <img className={styles.menuBtn}
+      <div
+        className={`${styles.menu} ${scrolled ? styles.scrolled : ""} ${menuOpen ? styles.menuExpanded : ""}`}
+      >
+        <img
+          className={styles.menuBtn}
           src={
             menuOpen
               ? getImageUrl("navIcon/closeIcon.svg")
@@ -45,13 +36,10 @@ export const Navbar = () => {
             <Link to="/portfolio">All Work</Link>
           </li>
           <li onClick={() => setMenuOpen(false)}>
-  <Link to="/about">About</Link>
-</li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
-
       </div>
     </nav>
   );
 };
-
-
